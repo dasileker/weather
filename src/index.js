a3e08ca8b2626339220f68b3e69d13dedde8b454
dasileker <dasileker@gmail.com>
2023-04-05 21:57:46 +0000

    add forcast for 4 days

--- src/index.js
+++ src/index.js
@@ -9,13 +9,51 @@ message.innerHTML = '';
 
 const getReport = async (city) => {
   try {
-    const response = await fetch(
+    // Retrieve the current weather data for the given city
+    const currentResponse = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApi.key}&units=metric`,
     );
+    const currentResult = await currentResponse.json();
+    const currentWeather = currentResult;
 
-    const result = await response.json();
-    const weather = result;
-    showWeatherReport(weather);
+    // Retrieve the 5-day forecast for the given city
+    const forecastResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherApi.key}&units=metric`,
+    );
+    const forecastResult = await forecastResponse.json();
+    const forecastWeather = forecastResult.list;
+
+    // Display the current weather data and 5-day forecast
+    showWeatherReport(currentWeather);
+
+    const forecastDiv = document.createElement('div');
+    forecastDiv.classList.add('forecast');
+
+    for (let i = 0; i < forecastWeather.length; i += 8) {
+      const dayData = forecastWeather[i];
+
+      const forecastDay = document.createElement('div');
+      forecastDay.classList.add('forecast-day');
+
+      const forecastDate = document.createElement('div');
+      forecastDate.classList.add('forecast-date');
+      forecastDate.innerText = new Date(dayData.dt_txt).toLocaleDateString();
+
+      const forecastIcon = document.createElement('img');
+      forecastIcon.classList.add('forecast-icon');
+      forecastIcon.src = `https://openweathermap.org/img/w/${dayData.weather[0].icon}.png`;
+
+      const forecastTemp = document.createElement('div');
+      forecastTemp.classList.add('forecast-temp');
+      forecastTemp.innerText = `${Math.round(dayData.main.temp)}°C`;
+
+      forecastDay.append(forecastDate, forecastIcon, forecastTemp);
+      forecastDiv.append(forecastDay);
+    }
+
+    const weatherDiv = document.querySelector('.weather');
+    weatherDiv.parentNode.insertBefore(forecastDiv, weatherDiv.nextSibling);
+
   } catch (error) {
     message.innerHTML = `this ${error}`;
   }
