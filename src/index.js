4e4312e8b66d2efe10d0fdb8acdd2093735a2fb2
dasileker <dasileker@gmail.com>
2023-04-07 13:28:24 +0000

    call the forcats.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import showWeatherReport from './weather';
+import showForecast from './forcast';
 
 const weatherApi = {
   key: '2ac2a201f9a298fd6ce2b569bf302448',
@@ -24,35 +25,9 @@ const getReport = async (city) => {
     const forecastWeather = forecastResult.list;
 
     // Display the current weather data and 5-day forecast
+    
     showWeatherReport(currentWeather);
-
-    const forecastContainer = document.createElement('div');
-    forecastContainer.classList.add('forecast-container');
-
-    for (let i = 0; i < forecastWeather.length; i += 8) {
-      const dayData = forecastWeather[i];
-
-      const forecastCard = document.createElement('div');
-      forecastCard.classList.add('forecast-card');
-
-      const forecastDate = document.createElement('div');
-      forecastDate.classList.add('forecast-date');
-      forecastDate.innerText = new Date(dayData.dt_txt).toLocaleDateString();
-
-      const forecastIcon = document.createElement('img');
-      forecastIcon.classList.add('forecast-icon');
-      forecastIcon.src = `https://openweathermap.org/img/w/${dayData.weather[0].icon}.png`;
-
-      const forecastTemp = document.createElement('div');
-      forecastTemp.classList.add('forecast-temp');
-      forecastTemp.innerText = `${Math.round(dayData.main.temp)}°C`;
-
-      forecastCard.append(forecastDate, forecastIcon, forecastTemp);
-      forecastContainer.append(forecastCard);
-    }
-
-    const weatherDiv = document.querySelector('.weather');
-    weatherDiv.parentNode.insertBefore(forecastContainer, weatherDiv.nextSibling);
+    showForecast(forecastWeather);
 
   } catch (error) {
     message.innerHTML = `this ${error}`;
